7ac127362a95359e1fb2b6f99a225dab053c4e10
ShererStress <ryanswhitehill@gmail.com>
2019-06-21 01:20:21 -0600

    React rebuilt with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      someData: "THE PAGE IS FUNCTIONAL",
+    };
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> {this.state.someData} </h1>
+        <h2> Some text here </h2>
+      </div>
+    );
+  }
+
 }
 
 export default App;
