c55331069d4f432c1157c3df6393a8a41b9fd9d9
ShererStress <ryanswhitehill@gmail.com>
2019-06-21 11:45:53 -0600

    Got a download link working. Adding user login/create form

--- src/App.js
+++ src/App.js
@@ -8,14 +8,36 @@ class App extends Component {
 
     this.state = {
       someData: "THE PAGE IS FUNCTIONAL",
+      loggedUseData: {
+        username: null,
+        mostrecentver: null,
+      },
+      formData: {
+        username: "",
+        password: "",
+      },
     };
   };
+
+
+  //Add a random text file to download via a link
+  //Connect to backend with create/login/logout
+  //Check for current version
 
   render() {
     return (
       <div>
         <h1> {this.state.someData} </h1>
         <h2> Some text here </h2>
+        <a href="./downloads/testReadme.txt" download>Download File</a>
+
+        <form>
+          Username: <input type="text"></input> <br/>
+          Password: <input type="password"></input> <br/>
+          <button> LogIn </button>
+        </form>
+
+
       </div>
     );
   }
